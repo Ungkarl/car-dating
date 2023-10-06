b0a268de8798cdaed291c7149c8229daec3c6a6d
AndreasBP20 <andreasbpedersen12@gmail.com>
2023-10-06 12:46:33 +0200

    Update data.service.js

--- js/data.service.js
+++ js/data.service.js
@@ -2,7 +2,7 @@ const service = {};
 
 service.getAccounts = async () => {
     try {
-        const response = await fetch('/data/accounts.json')
+        const response = await fetch('https://ungkarl.github.io/data/accounts.json')
         const accounts = await response.json();
         return accounts;
     }
